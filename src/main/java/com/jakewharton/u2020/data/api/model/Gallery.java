13d77f0a1670cd8f30cef4575604ead8cdfde353
Jake Wharton <jw@squareup.com>
2014-01-22 12:32:20 -0800

    final

--- src/main/java/com/jakewharton/u2020/data/api/model/Gallery.java
+++ src/main/java/com/jakewharton/u2020/data/api/model/Gallery.java
@@ -2,7 +2,7 @@ package com.jakewharton.u2020.data.api.model;
 
 import java.util.List;
 
-public class Gallery extends ImgurResponse {
+public final class Gallery extends ImgurResponse {
   public final List<Image> data;
 
   public Gallery(int status, boolean success, List<Image> data) {
