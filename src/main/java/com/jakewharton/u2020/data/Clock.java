9af47e4dd01c733ed2c51d8d27e6e31bb746e5ba
Matt Precious <mattp@squareup.com>
2015-03-26 00:31:50 -0400

    REAL clock

--- src/main/java/com/jakewharton/u2020/data/Clock.java
+++ src/main/java/com/jakewharton/u2020/data/Clock.java
@@ -3,4 +3,14 @@ package com.jakewharton.u2020.data;
 public interface Clock {
   long millis();
   long nanos();
+
+  Clock REAL = new Clock() {
+    @Override public long millis() {
+      return System.currentTimeMillis();
+    }
+
+    @Override public long nanos() {
+      return System.nanoTime();
+    }
+  };
 }
