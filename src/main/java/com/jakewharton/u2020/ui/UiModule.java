555b1daa72f378c58ca587fe5067610d02c7b31c
Jake Wharton <jw@squareup.com>
2015-03-27 15:12:13 -0400

    Add simple error screen.

--- src/main/java/com/jakewharton/u2020/ui/UiModule.java
+++ src/main/java/com/jakewharton/u2020/ui/UiModule.java
@@ -1,14 +1,9 @@
 package com.jakewharton.u2020.ui;
 
 import com.jakewharton.u2020.ui.trending.TrendingView;
-import com.jakewharton.u2020.util.ViewSubscriptions;
 import dagger.Module;
 import dagger.Provides;
-import javax.inject.Named;
 import javax.inject.Singleton;
-import rx.Scheduler;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 @Module(
     injects = {
@@ -25,19 +20,5 @@ public final class UiModule {
 
   @Provides @Singleton ActivityHierarchyServer provideActivityHierarchyServer() {
     return ActivityHierarchyServer.NONE;
-  }
-
-  @Provides @Singleton @Named("subscribeOn") Scheduler provideSubscribeOnScheduler() {
-    return Schedulers.io();
-  }
-
-  @Provides @Singleton @Named("observeOn") Scheduler provideObserveOnScheduler() {
-    return AndroidSchedulers.mainThread();
-  }
-
-  @Provides ViewSubscriptions provideViewSubscriptions(
-      @Named("subscribeOn") Scheduler subscribeOnScheduler,
-      @Named("observeOn") Scheduler observeOnScheduler) {
-    return new ViewSubscriptions(subscribeOnScheduler, observeOnScheduler);
   }
 }
