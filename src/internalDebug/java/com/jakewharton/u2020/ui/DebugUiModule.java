6241f3a356483fc8a39e5f6b9b2173a883c69513
Matt Precious <mattp@squareup.com>
2015-04-02 13:31:07 -0400

    Move debug components into a DebugView

--- src/internalDebug/java/com/jakewharton/u2020/ui/DebugUiModule.java
+++ src/internalDebug/java/com/jakewharton/u2020/ui/DebugUiModule.java
@@ -1,13 +1,17 @@
 package com.jakewharton.u2020.ui;
 
 import com.jakewharton.u2020.ui.debug.DebugAppContainer;
+import com.jakewharton.u2020.ui.debug.DebugView;
 import com.jakewharton.u2020.ui.debug.SocketActivityHierarchyServer;
 import dagger.Module;
 import dagger.Provides;
 import javax.inject.Singleton;
 
 @Module(
-    injects = DebugAppContainer.class,
+    injects = {
+        DebugAppContainer.class,
+        DebugView.class,
+    },
     complete = false,
     library = true,
     overrides = true
