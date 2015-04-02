9ee72a098597a689b8938914de614f99c48ca9a7
Jake Wharton <jw@squareup.com>
2015-04-02 14:50:02 -0400

    Use mock avatars for mock data.

--- src/internalDebug/java/com/jakewharton/u2020/data/api/MockUsers.java
+++ src/internalDebug/java/com/jakewharton/u2020/data/api/MockUsers.java
@@ -3,12 +3,7 @@ package com.jakewharton.u2020.data.api;
 import com.jakewharton.u2020.data.api.model.User;
 
 final class MockUsers {
-  static final User JAKE_WHARTON =
-      new User("JakeWharton", "https://avatars.githubusercontent.com/u/66577?v=3");
-
-  static final User SQUARE =
-      new User("square", "https://avatars.githubusercontent.com/u/82592?v=3");
-
-  static final User MATT_PRECIOUS =
-      new User("mattprecious", "https://avatars.githubusercontent.com/u/333672?v=3");
+  static final User SQUARE = new User("square", "mock:///avatar/one.png");
+  static final User JAKE_WHARTON = new User("JakeWharton", "mock:///avatar/two.png");
+  static final User MATT_PRECIOUS = new User("mattprecious", "mock:///avatar/three.png");
 }
