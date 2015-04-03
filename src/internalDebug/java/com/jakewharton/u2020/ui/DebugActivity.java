da282e47c057ea6bcfec9feeff959195a822ac49
Jake Wharton <jw@squareup.com>
2015-04-03 01:36:03 -0400

    Add new injector support to debug activity.

--- src/internalDebug/java/com/jakewharton/u2020/ui/DebugActivity.java
+++ src/internalDebug/java/com/jakewharton/u2020/ui/DebugActivity.java
@@ -2,11 +2,24 @@ package com.jakewharton.u2020.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import com.jakewharton.u2020.R;
+import com.jakewharton.u2020.data.Injector;
+import dagger.ObjectGraph;
 
 public final class DebugActivity extends Activity {
+  private ObjectGraph appGraph;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    appGraph = Injector.obtain(getApplication());
     setContentView(R.layout.debug_activity);
+  }
+
+  @Override public Object getSystemService(@NonNull String name) {
+    if (Injector.matchesService(name)) {
+      return appGraph;
+    }
+    return super.getSystemService(name);
   }
 }
