a2c25ce80cc0daacef02fdaf88755ed16c43821c
Prateek Srivastava <f2prateek@gmail.com>
2015-09-17 10:33:29 -0700

    Instrument Spoon

--- src/androidTestInternal/java/com/jakewharton/u2020/ui/DummyTest.java
+++ src/androidTestInternal/java/com/jakewharton/u2020/ui/DummyTest.java
@@ -5,6 +5,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import com.squareup.spoon.Spoon;
 
 import static android.os.SystemClock.sleep;
 import static java.util.concurrent.TimeUnit.SECONDS;
@@ -16,7 +17,9 @@ public final class DummyTest {
       new ActivityTestRule<>(MainActivity.class);
 
   @Test public void noneOfTheThings() {
+    Spoon.screenshot(main.getActivity(), "initial_state");
     sleep(SECONDS.toMillis(5)); // Long enough to see some data from mock mode.
     assertTrue(true);
+    Spoon.screenshot(main.getActivity(), "displayed_data");
   }
 }
