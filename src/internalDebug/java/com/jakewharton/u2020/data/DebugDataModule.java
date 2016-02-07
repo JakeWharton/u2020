f2ceca53b947b9aa799a96c575c3ee326ad53b03
Jake Wharton <jw@squareup.com>
2016-02-06 23:43:47 -0500

    OkHttp 3.1

--- src/internalDebug/java/com/jakewharton/u2020/data/DebugDataModule.java
+++ src/internalDebug/java/com/jakewharton/u2020/data/DebugDataModule.java
@@ -163,7 +163,7 @@ public final class DebugDataModule {
         }
 
         @Override public X509Certificate[] getAcceptedIssuers() {
-          return null;
+          return new X509Certificate[0];
         }
       };
       context.init(null, new TrustManager[] { permissive }, null);
