ec4a4c0d1949d4898b06b3b2af665c4d32d0e7e8
Jake Wharton <jakewharton@gmail.com>
2017-07-19 14:21:36 -0400

    Break out GitHub service module.

--- service/github/src/main/java/com/jakewharton/u2020/data/api/model/User.java
+++ service/github/src/main/java/com/jakewharton/u2020/data/api/model/User.java
@@ -3,14 +3,14 @@ package com.jakewharton.u2020.data.api.model;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import static com.jakewharton.u2020.util.Preconditions.checkNotNull;
+import static java.util.Objects.requireNonNull;
 
 public final class User {
   @NonNull public final String login;
   @Nullable public final String avatar_url;
 
   public User(String login, @Nullable String avatar_url) {
-    this.login = checkNotNull(login, "login == null");
+    this.login = requireNonNull(login, "login == null");
     this.avatar_url = avatar_url;
   }
 
